137c2a14c2384775ada8c9bd1cc270a82824cd5b
Mags Cousins <47702701+magscousins@users.noreply.github.com>
2023-10-11 12:41:09 +0000

    Make file and container

---
+++ src/david-kano/main.py
@@ -0,0 +1,9 @@
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+if __name__ == "__main__":
+    run()
